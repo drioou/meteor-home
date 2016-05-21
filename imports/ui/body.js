f5919a2d78098149b88cfcd99491d65dc5665a63
drioou <1406422800@qq.com>
2016-05-21 22:49:24 +0800

    start

---
+++ imports/ui/body.js
@@ -0,0 +1,28 @@
+import { Template } from 'meteor/templating';
+ 
+import { Tasks } from '../api/tasks.js';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
+
+Template.body.events({
+	'submit .new-task'(event){
+
+		event.preventDefault();
+
+		const target = event.target;
+
+		const text = target.text.value;
+		
+		Tasks.insert({
+			text,
+			createAt: new Date(),
+		});
+		target.text.value = '';
+	},
+});
